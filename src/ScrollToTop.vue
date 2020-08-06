e6c7e37c1550ec97a0218abdfc406aeccc31335f
asdf1899 <araid.anas99@gmail.com>
2020-08-06 16:04:00 +0200

    Add scroll and fix things

---
+++ src/ScrollToTop.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="vue-div-wrapper">
+    <button class="vue-scroll-button" v-if="isVisible" @click="goToTop(duration)">{{this.text}}</button>
+  </div>
+</template>
+
+<script>
+  export default{
+    name: 'ScrollToTop',
+    props: {
+      text: {
+        type: String,
+        default: 'Back to top',
+      },
+      visibleY:{
+        type: Number,
+        default: 300,
+      },
+      duration:{
+        type: Number,
+        default: 1000,
+      }
+    },
+    data(){
+      return {
+        isVisible: false
+      }
+    },
+    created () {
+      window.addEventListener('scroll', this.scrollEvent);
+    },
+    destroyed () {
+      window.removeEventListener('scroll', this.scrollEvent);
+    },
+    methods: {
+      scrollEvent () {
+        this.isVisible = (this.visibleY < window.scrollY);
+      },
+      goToTop(duration){
+        if (document.scrollingElement.scrollTop === 0) return;
+        const totalScrollDistance = document.scrollingElement.scrollTop;
+        let scrollY = totalScrollDistance
+        let oldTimestamp = null;
+        //==========================================
+        // thanks stackoverflow for this function ¯\_(ツ)_/¯
+        function step (newTimestamp) {
+            if (oldTimestamp !== null) {
+                scrollY -= totalScrollDistance * (newTimestamp - oldTimestamp) / duration;
+                if (scrollY <= 0) return document.scrollingElement.scrollTop = 0;
+                document.scrollingElement.scrollTop = scrollY;
+            }
+            oldTimestamp = newTimestamp;
+            window.requestAnimationFrame(step);
+        }
+        window.requestAnimationFrame(step);
+        //==========================================
+      }
+    }
+  }
+</script>
+
+<style>
+.vue-scroll-button{
+  cursor:pointer;
+  position: fixed;
+  z-index: 1000;
+  margin-top:30px;
+}
+.vue-div-wrapper{
+  text-align:center;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+</style>
