827d8b22d69c5d0fd490852eefa0257422e85021
asdf1899 <araid.anas99@gmail.com>
2020-08-06 14:55:27 +0200

    Init project

--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
-import Vue from 'vue'
-import App from './App.vue'
+import component from './ScrollToTop.vue';
 
-Vue.config.productionTip = false
+function install(Vue) {
+  if (install.installed) return;
+  install.installed = true;
+  Vue.component("vue-scroll-to-top", component);
+}
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+const plugin = {
+  install
+};
+
+let GlobalVue = null;
+if (typeof window !== "undefined") {
+  GlobalVue = window.Vue;
+} else if (typeof global !== "undefined") {
+  GlobalVue = global.vue;
+}
+
+if (GlobalVue) {
+  GlobalVue.use(plugin);
+}
+
+component.install = install;
+
+export default component;
